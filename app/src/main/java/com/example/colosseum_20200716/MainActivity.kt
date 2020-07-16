829ed68ca97d123418aa8181a61c568dfa910a39
이예원 <gokaiko@naver.com>
2020-07-16 20:16:17 +0900

    Start

--- app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
+++ app/src/main/java/com/example/colosseum_20200716/MainActivity.kt
@@ -3,9 +3,19 @@ package com.example.colosseum_20200716
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+
 }
