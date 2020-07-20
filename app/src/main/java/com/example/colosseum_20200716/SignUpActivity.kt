2a2b8cf5490ea393a5cf139dd5280b552cfc98cc
이예원 <gokaiko@naver.com>
2020-07-20 20:20:02 +0900

    SignUp activity_sign_up.xml - added edttexts and textviews SignUpActivity.kt - implemented passwordEdt onclickEvent

--- app/src/main/java/com/example/colosseum_20200716/SignUpActivity.kt
+++ app/src/main/java/com/example/colosseum_20200716/SignUpActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.widget.Toast
+import com.example.colosseum_20200716.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -18,23 +21,80 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-//  비밀번호 입력내용 변경 이벤트 처리
-        passwordEdt.addTextChangedListener(object: TextWatcher{
-            override fun afterTextChanged(p0: Editable?) {
+
+        okBtn.setOnClickListener {
+
+//            닉네임 / 이메일은 비어있으면 안된다.
+
+            val inputEmail = emailEdt.text.toString()
+
+            if (inputEmail.isEmpty()) {
+                Toast.makeText(mContext, "이메일은 반드시 입력해야합니다.", Toast.LENGTH_SHORT).show()
+//                밑의 코드는 실행할 필요가 없다.
+//                이벤트 처리를 강제 종료.
+                return@setOnClickListener
+            }
+
+            val inputNickName = inputNickname.text.toString()
+
+            if (inputNickName.isEmpty()) {
+                Toast.makeText(mContext, "닉네임은 반드시 입력해야합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            비번은 8글자 이내면 안됨.
+
+            val inputPassword = inputPassword.text.toString()
+
+            if (inputPassword.length < 8) {
+                Toast.makeText(mContext, "비밀번호는 최소 8글자 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            모든 검사를 통과하면, 서버에 가입 요청
+//            여기 코드가 실행된다 => 모든 검사를 통과했다.
+
+//            ServerUtil.
+
+
+        }
+
+//        비밀번호 입력 내용 변경 이벤트 처리
+        inputPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
 
             }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
 
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
- //         내용 변경 완료된 시점에 실행
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                내용 변경 완료된 시점에 실행
 
-                Log.d("비번 입력", p0.toString())
+//                입력된 글자의 길이 확인.
+//                비어있다면, "비밀번호를 입력해 주세요."
+//                8글자 안되면, "비밀번호가 너무 짧습니다."
+//                그 이상이면, "사용해도 좋은 비밀번호입니다."
+
+                val tempPw = inputPassword.text.toString()
+
+                if (tempPw.isEmpty()) {
+//                    입력 안한 경우
+                    passwordCheckResultTxt.text = "비밀번호를 입력해 주세요."
+                } else if (tempPw.length < 8) {
+//                    길이가 부족한 경우
+                    passwordCheckResultTxt.text = "비밀번호가 너무 짧습니다."
+                } else {
+//                    충분히 긴 비밀번호
+                    passwordCheckResultTxt.text = "사용해도 좋은 비밀번호 입니다."
+                }
+
+
             }
 
         })
+
     }
 
     override fun setValues() {
--- app/src/main/java/com/example/colosseum_20200716/utils/ServerUtil.kt
+++ app/src/main/java/com/example/colosseum_20200716/utils/ServerUtil.kt
@@ -22,7 +22,7 @@ class ServerUtil {
 
 //      로그인 API를 호출해 주는 기능
 //      1.화면에서 어떤 데이터를 받아와야 하는지? email, pw
-        fun postRequestLogin(context: Context, email:String, pw:String, handler: JsonResponseHandler?) {
+        fun postRequestLogin(context: Context, email:String, password:String, handler: JsonResponseHandler?) {
 //      서버 통신 담당 변수(클라이언트 역할 수행 변수)
             val client = OkHttpClient()
 //      어느 주소로 가야하는지 저장(http://15.165.177.142/user로 가자)
@@ -31,7 +31,7 @@ class ServerUtil {
 //      POST / PUT/ PATCH 가 같은 방식
             val formData = FormBody.Builder()
                 .add("email",email)
-                .add("password", pw)
+                .add("password", password)
                 .build()
 //      요청 정보를 종합하는 변수 Request 사용
 //      Intent 를 만드는것과 비슷한 개념
@@ -58,6 +58,49 @@ class ServerUtil {
                      Log.d("서버응답내용",json.toString())
 //                    handler 변수에 응답 처리 코드가 들어있다면 실행해주자.
                      handler?.onResponse(json)
+                }
+
+            })
+        }
+
+        //회원가입 호출기능
+        fun putRequestSignUp(context: Context, email:String, password: String, nickname: String,handler: JsonResponseHandler?) {
+//      서버 통신 담당 변수(클라이언트 역할 수행 변수)
+            val client = OkHttpClient()
+//      어느 주소로 가야하는지 저장(http://15.165.177.142/user로 가자)
+            val urlString = "${BASE_URL}/user"
+//      서버에 가지고 갈 짐(데이터들) 을 FormBody를 이용해서 담자
+//      POST / PUT/ PATCH 가 같은 방식
+            val formData = FormBody.Builder()
+                .add("email",email)
+                .add("password", password)
+                .add("nickname", nickname)
+                .build()
+//      요청 정보를 종합하는 변수 Request 사용
+//      Intent 를 만드는것과 비슷한 개념
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+//      종합된 request를 이용해서 실제 API 호출(누가? client가)
+//      받아올 응답도 같이 처리
+
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+//              서버 연결 자체에 실패한 경우
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+//             연결은 성공해서, 서버가 응답을 내려줬을 때 실행됨
+//             실제로 서버가 내려준 응답 내용을 변수로 저장
+                    val bodyStr = response.body?.string()
+//                   응답내용으로  Json 객체 생성
+                    val json = JSONObject(bodyStr)
+//                   최종적으로 가져온 내용을 로그로 출력
+                    Log.d("서버응답내용",json.toString())
+//                    handler 변수에 응답 처리 코드가 들어있다면 실행해주자.
+                    handler?.onResponse(json)
                 }
 
             })
