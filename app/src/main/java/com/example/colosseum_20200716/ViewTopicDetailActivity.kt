9f4f0122e4f6cecf5f0d9a47f9b20404b5127eae
이예원 <gokaiko@naver.com>
2020-07-21 21:40:40 +0900

    side class + json Parsing

--- app/src/main/java/com/example/colosseum_20200716/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/example/colosseum_20200716/ViewTopicDetailActivity.kt
@@ -19,7 +19,8 @@ class ViewTopicDetailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_topic_detail)
-
+        setupEvents()
+        setValues()
     }
 
     override fun setupEvents() {
