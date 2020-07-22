b33cf5e7d4e13e93745c5ee29e22a5fe34076f6d
이예원 <gokaiko@naver.com>
2020-07-22 20:50:42 +0900

    parsing quiz

--- app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
+++ app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
@@ -1,4 +1,12 @@
 package com.example.colosseum_20200716.datas
 
 class Reply {
+    var id = 0
+    var content = ""
+    lateinit var writer : User
+    lateinit var selectedSide : Side
+
+    companion object{
+        //JSONObject 하나를 넣으면 의견내용을 파싱해서  Reply 로 리턴하는 기능
+    }
 }
