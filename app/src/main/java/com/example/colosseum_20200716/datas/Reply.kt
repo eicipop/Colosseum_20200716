fcd90b262e66b2da25f289f43f352085ac4c9369
이예원 <gokaiko@naver.com>
2020-07-22 21:03:46 +0900

    user's opinion parsing added

--- app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
+++ app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
@@ -1,5 +1,7 @@
 package com.example.colosseum_20200716.datas
 
+import org.json.JSONObject
+
 class Reply {
     var id = 0
     var content = ""
@@ -8,5 +10,20 @@ class Reply {
 
     companion object{
         //JSONObject 하나를 넣으면 의견내용을 파싱해서  Reply 로 리턴하는 기능
+        fun getReplyFromJson(json: JSONObject) : Reply {
+
+//            변환시켜줄 Topic 객체 생성
+            val r = Reply()
+
+//            만든 객체의 내용물들을 json을 이용해서 채우자
+            r.id = json.getInt("id")
+            r.content = json.getString("content")
+            // 작성자 / 선택진영 => JSONObject를 받아서 곧바로 대입
+            r.writer = User.getUserFromJson(json.getJSONObject("user"))
+
+            r.selectedSide = Side.getSideFromJson(json.getJSONObject("selected_side"))
+
+            return r
+        }
     }
 }
--- app/src/main/java/com/example/colosseum_20200716/datas/User.kt
+++ app/src/main/java/com/example/colosseum_20200716/datas/User.kt
@@ -12,7 +12,17 @@ class User {
         fun getUserFromJson(json : JSONObject) : User{
             val u = User()
             // 사용자 정보파싱하는 코드
+
+            //            만든 객체의 내용물들을 json을 이용해서 채우자
+            u.id = json.getInt("id")
+            u.email = json.getString("email")
+            u.nickName = json.getString("nick_name")
+
+//            sides 배열에 들어있는, 진영 선택 정보도 넣어줘야함.
+
+
             return  u
         }
+
     }
 }
