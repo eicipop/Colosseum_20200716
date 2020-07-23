bd67514b9d9054df38f93b75db9825fc2acac432
이예원 <gokaiko@naver.com>
2020-07-23 22:07:08 +0900

    event operated from inner button in listView - using mContext

--- app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.colosseum_20200716.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import com.example.colosseum_20200716.R
+import com.example.colosseum_20200716.ViewReplyDetailActivity
 import com.example.colosseum_20200716.datas.Reply
 import com.example.colosseum_20200716.utils.TimeUtil
 import java.text.SimpleDateFormat
@@ -49,7 +51,16 @@ class ReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) 
         dislikeBtn.text = "싫어요 ${data.dislikecount}"
         replyBtn.text = "답글 ${data.replyCount}"
 
+        //답글 버튼이 눌리면 의견 상세 화면으로 진입
+        replyBtn.setOnClickListener {
 
+            val myIntent = Intent(mContext, ViewReplyDetailActivity::class.java)
+            // startActivity 함수는 AppCompatActivity 가 내려주는 기능.
+            // Adapter는 액티비티가 아니므로, startActivity 기능을 내려주지 않는다.
+            // mContext 변수가, 어떤 화면이 리스트뷰를 뿌리는지 들고 있다.
+            // mContext를 이용해서 액티비티를 열어주자.
+            mContext.startActivity(myIntent)
+        }
         return row
     }
 }
