dc7f44066cc2df2ea1c4117defe59e0f366ccd42
이예원 <gokaiko@naver.com>
2020-07-23 21:21:06 +0900

    3 buttons added in reply_list_item.xml

--- app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -8,35 +8,51 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.example.colosseum_20200716.R
 import com.example.colosseum_20200716.datas.Reply
+import com.example.colosseum_20200716.utils.TimeUtil
 import java.text.SimpleDateFormat
 
 
-class ReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList){
+class ReplyAdapter(
+    val mContext:Context,
+    resId: Int,
+    val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
-        if(tempRow == null){
+
+        if (tempRow == null) {
             tempRow = inf.inflate(R.layout.reply_list_item, null)
         }
+
         val row = tempRow!!
+
         val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
-        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
         val selectedSideTitleTxt = row.findViewById<TextView>(R.id.selectedSideTitleTxt)
-        var replywriteTimeTxt = row.findViewById<TextView>(R.id.replayWriteTimeTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+
+//        시간 정보 텍스트뷰
+        val replyWriteTimeTxt = row.findViewById<TextView>(R.id.replayWriteTimeTxt)
 
         val data = mList[position]
 
         writerNickNameTxt.text = data.writer.nickName
-        contentTxt.text = data.content
         selectedSideTitleTxt.text = "(${data.selectedSide.title})"
+        contentTxt.text = data.content
 
-        // 시간정보 텍스트뷰 내용 설정 -> 방금전, ?분전, ?시간 전 등등..
-        // 날짜 출력 양식용 변수
-        val sdf = SimpleDateFormat("yy-MM-dd a h시 m분")
-        replywriteTimeTxt.text = sdf.format(data.writtenDateTime.time)
+//        시간정보 텍스트뷰 내용 설정 => 방금 전, ?분 전, ?시간 전 등등..
+
+        replyWriteTimeTxt.text = TimeUtil.getTimeAgoFromCalendar(data.writtenDateTime)
+
+
+//        날짜 출력 양식용 변수
+//        val sdf = SimpleDateFormat("yy-MM-dd a h시 m분")
+//
+//        replyWriteTimeTxt.text = sdf.format(data.writtenDateTime.time)
+
 
         return row
     }
+
 }
