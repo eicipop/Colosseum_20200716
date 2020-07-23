3f1355f15560e9446769497769ba0df67621cb32
이예원 <gokaiko@naver.com>
2020-07-23 20:59:11 +0900

    timezone calibration between server and phone

--- app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
+++ app/src/main/java/com/example/colosseum_20200716/datas/Reply.kt
@@ -33,6 +33,19 @@ class Reply {
 
             // 멤버변수인 Calendar 변수에게 데이터 적용
             r.writtenDateTime.time = sdf.parse(createdAtString)
+
+            // 핸드폰의 시간대와, 서버시간대의 시차를 구해서
+            // 작성 일시의 시간값을 조정
+            // 내 폰의 시간대가 어디 시간대인지 변수로 저장
+            val myPhoneTimeZone = r.writtenDateTime.timeZone  // 한국폰 : 한국시간대
+            // (서버랑) 몇시간 차이가 나는지 변수로 저장. -> 밀리초까지 계산된 시차 -> 시간 단위로 변경
+            val timeOffset = myPhoneTimeZone.rawOffset / 1000 / 60 / 60
+            // 게시글 작성시간을 timeOffset만큼 시간값을 더해주자.
+            r.writtenDateTime.add(Calendar.HOUR, timeOffset)
+
+            //
+
+
             return r
         }
     }
