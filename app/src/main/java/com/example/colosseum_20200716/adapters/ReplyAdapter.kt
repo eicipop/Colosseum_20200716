9091bfebff395922f99f58276ebc934e52e2f29e
이예원 <gokaiko@naver.com>
2020-07-23 22:01:38 +0900

    reply View added

--- app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_20200716/adapters/ReplyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
 import com.example.colosseum_20200716.R
 import com.example.colosseum_20200716.datas.Reply
@@ -25,8 +26,12 @@ class ReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) 
         val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
         val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
         val selectedSideTitleTxt = row.findViewById<TextView>(R.id.selectedSideTitleTxt)
+        // 시간정보 텍스트뷰
         var replywriteTimeTxt = row.findViewById<TextView>(R.id.replyWriteTimeTxt)
-
+        //댓/좋/실 버튼 추가
+        val replyBtn = row.findViewById<Button>(R.id.replyBtn)
+        val likeBtn = row.findViewById<Button>(R.id.likeBtn)
+        val dislikeBtn = row.findViewById<Button>(R.id.dislikeBtn)
         val data = mList[position]
 
         writerNickNameTxt.text = data.writer.nickName
@@ -39,6 +44,11 @@ class ReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) 
         // 날짜 출력 양식용 변수
 //        val sdf = SimpleDateFormat("yy-MM-dd a h시 m분")
 //        replywriteTimeTxt.text = sdf.format(data.writtenDateTime.time)
+        // 좋 /싫 / 답글 갯수 반영
+        likeBtn.text = "좋아요 ${data.likecount}"
+        dislikeBtn.text = "싫어요 ${data.dislikecount}"
+        replyBtn.text = "답글 ${data.replyCount}"
+
 
         return row
     }
