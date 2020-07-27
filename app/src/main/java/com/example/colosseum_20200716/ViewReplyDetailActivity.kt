cc5d6df4fd418b3d3a7d7d09350087dcb290feb3
이예원 <gokaiko@naver.com>
2020-07-27 20:20:05 +0900

    의견 작성시간 + 댓글 작성시간 파싱

--- app/src/main/java/com/example/colosseum_20200716/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/example/colosseum_20200716/ViewReplyDetailActivity.kt
@@ -2,12 +2,21 @@ package com.example.colosseum_20200716
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.colosseum_20200716.datas.Reply
+import com.example.colosseum_20200716.utils.ServerUtil
+import com.example.colosseum_20200716.utils.TimeUtil
+import kotlinx.android.synthetic.main.activity_view_reply_detail.*
+import kotlinx.android.synthetic.main.reply_list_item.*
+import org.json.JSONObject
 
 class ViewReplyDetailActivity : BaseActivity() {
 
-//  보려는 의견의 id는 여러 함수에서 공유할 것 같다.
+    //  보려는 의견의 id는 여러 함수에서 공유할 것 같다.
 //  그래서 멤버변수로 만들고 저장한다.
     var mReplyId = 0
+
+    // 이 화면에서 보여줘야할 의견의 정보를 가진 변수 -> 멤버변수
+    lateinit var mReply: Reply
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +25,7 @@ class ViewReplyDetailActivity : BaseActivity() {
         setupEvents()
         setValues()
     }
+
     override fun setupEvents() {
 
     }
@@ -23,7 +33,33 @@ class ViewReplyDetailActivity : BaseActivity() {
     override fun setValues() {
 //  의견 리스트뷰에서 보내준 id값을 멤버변수에 담아주자.
         mReplyId = intent.getIntExtra("replyId", 0)
+
+//  해당 id 값에 맞는 의견 정보를 다시 불러오자
+        getReplyFromServer()
+
     }
 
+    // 서버에서 의견 정보 불러오기
+    fun getReplyFromServer() {
+        ServerUtil.getRequestReplyDetail(
+            mContext,
+            mReplyId,
+            object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+                    val data = json.getJSONObject("data")
+                    val replyObj = data.getJSONObject("reply")
+                    // replyObj 를 Reply클래스로 변환 -> mReplay 에 저장
+                    mReply = Reply.getReplyFromJson(replyObj)
 
+                    //mReply 내부의 변수(정보)들을 화면에 반영
+                    runOnUiThread {
+                        writerNickNameTxt.text = mReply.writer.nickName
+                        selectedSideTitleTxt.text = "(${mReply.selectedSide.title})"
+                        writtenDateTimeTxt.text = TimeUtil.getTimeAgoFromCalendar(mReply.writtenDateTime)
+                        replyContentTxt.text = mReply.content
+                    }
+                }
+
+            })
+    }
 }
