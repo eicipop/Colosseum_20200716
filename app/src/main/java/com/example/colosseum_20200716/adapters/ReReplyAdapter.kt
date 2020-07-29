88f0dba923710eeafc3956895b56caa4b73ad7c8
eicipop <gokaiko@naver.com>
2020-07-29 15:47:45 +0900

    대댓글도  좋아요 싫어요 버튼 수정.누를때마다 둘다 동시에 숫자 변경되도록

--- app/src/main/java/com/example/colosseum_20200716/adapters/ReReplyAdapter.kt
+++ app/src/main/java/com/example/colosseum_20200716/adapters/ReReplyAdapter.kt
@@ -2,26 +2,32 @@ package com.example.colosseum_20200716.adapters
 
 import android.content.Context
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.colosseum_20200716.R
 import com.example.colosseum_20200716.ViewReplyDetailActivity
 import com.example.colosseum_20200716.datas.Reply
+import com.example.colosseum_20200716.utils.ServerUtil
 import com.example.colosseum_20200716.utils.TimeUtil
+import org.json.JSONObject
 import java.text.SimpleDateFormat
 
 
-class ReReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList){
+class ReReplyAdapter(val mContext: Context, val resId: Int, val mList: List<Reply>) :
+    ArrayAdapter<Reply>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView
-        if(tempRow == null){
+        if (tempRow == null) {
             tempRow = inf.inflate(R.layout.re_reply_list_item, null)
         }
         val row = tempRow!!
@@ -47,7 +53,121 @@ class ReReplyAdapter(val mContext: Context, val resId:Int, val mList:List<Reply>
         likeBtn.text = "좋아요 ${data.likecount}"
         dislikeBtn.text = "싫어요 ${data.dislikecount}"
 
+        // 좋 /싫 / 답글 갯수 반영
+        likeBtn.text = "좋아요 ${data.likecount}"
+        dislikeBtn.text = "싫어요 ${data.dislikecount}"
 
+        //내 좋/실 여부 반영
+        if(data.myLike) {
+            likeBtn.setBackgroundResource(R.drawable.red_border_box)
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.red))
+        }else{
+            likeBtn.setBackgroundResource(R.drawable.gray_border_box)
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
+        }
+        if(data.myDislike){
+            //싫어요 누른상태
+            dislikeBtn.setBackgroundResource(R.drawable.blue_border_box)
+            dislikeBtn.setTextColor(mContext.resources.getColor(R.color.blue))
+        }else{
+            dislikeBtn.setBackgroundResource(R.drawable.gray_border_box)
+            dislikeBtn.setTextColor(mContext.resources.getColor(R.color.textGray))
+        }
+
+        likeBtn.setOnClickListener {
+            ServerUtil.postRequestReplyLikeOrDislike(
+                mContext,
+                data.id,
+                true,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+// 변경된 좋아요 갯수/ 실ㅎ요 갯수를 파악해서
+                        // 버튼 문구를 새로반영
+                        // 목록에 뿌려진 DATA의 좋아요 실어요 갯수 변경
+
+                        val dataObj = json.getJSONObject("data")
+                        val replyObj = dataObj.getJSONObject("reply")
+
+                        val reply = Reply.getReplyFromJson(replyObj)
+//              이미 화면에 뿌려져 있는 data 의 내용만 교체
+                        data.myLike = reply.myLike
+                        data.myDislike = reply.myDislike
+                        if (data.myDislike == true) {
+                            data.myDislike = false
+                        }
+                        data.dislikecount = reply.dislikecount
+                        data.likecount = reply.likecount
+
+
+                        //                  data의 값이 변경 => 리스트뷰를 구성하는 목록에 변경
+//                    => 어댑터.notifyDataSetChanged 실행해야함
+//                    => 이 코드는 어댑터 내부 => notifyDataSetChanged 가 내장되어 있다.
+//                    => 단순히 호출만 하면 끝.
+
+//                    새로고침도 => UI 변경 => runOnUiThread 등으로 UI쓰레드가 처리하도록 해야함.
+//                    어댑터는 runOnUiThread 기능이 내장되어 있지 않다.
+
+//                    Handler 를 이용해서 => UI쓰레드에 접근하게 하자.
+
+                        val uiHandler = Handler(Looper.getMainLooper())
+
+                        uiHandler.post {
+                            notifyDataSetChanged()
+
+//                        서버가 알려주는 메세지를 토스트로 출력
+                            val message = json.getString("message")
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                        }
+
+                    }
+
+                })
+        }
+
+
+        // 싫어요 버튼 누르면 서버에 전달 처리 갯수 반영 / 토스트로 출력
+        dislikeBtn.setOnClickListener {
+            ServerUtil.postRequestReplyLikeOrDislike(
+                mContext,
+                data.id,
+                false,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+                        val dataObj = json.getJSONObject("data")
+                        val replyObj = dataObj.getJSONObject("reply")
+
+                        val reply = Reply.getReplyFromJson(replyObj)
+//              이미 화면에 뿌려져 있는 data 의 내용만 교체
+
+                        if (data.myLike == true) {
+                            data.myDislike = false
+                        }
+                        data.likecount = reply.likecount
+                        data.dislikecount = reply.dislikecount
+
+                        data.myLike = reply.myLike
+                        data.myDislike = reply.myDislike
+
+                        val uiHandler = Handler(Looper.getMainLooper())
+
+                        uiHandler.post {
+                            notifyDataSetChanged()
+
+//                        서버가 알려주는 메세지를 토스트로 출력
+                            val message = json.getString("message")
+
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                })
+        }
+
+        // 의견에 대한 좋아요/싫어요 버튼 클릭 이벤트
+        return row
 
         return row
     }
